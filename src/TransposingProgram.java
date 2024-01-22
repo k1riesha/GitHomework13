ab2306b2b95e62b42b884e8059f01f2d3575a339
k1riesha <kiriesha999@gmail.com>
2024-01-22 18:51:09 +0200

    NewCommit

--- src/TransposingProgram.java
+++ src/TransposingProgram.java
@@ -1,25 +1,48 @@
 import java.util.Random;
 
 public class TransposingProgram {
-
     public static void main(String[] args) {
-
         Random random = new Random();
 
-        int m = random.nextInt(10);
-        int n = random.nextInt(10);
-        int[][] array1 = new int[m][n];
-        int[][] array2 = new int[n][m];
+        int m = 4;
+        int n = 4;
 
-        for (int i = 0; i < array1.length; i++) {
-            for (int j = 0; j < array1.length; j++) {
-                array1[i][j] = random.nextInt(10);
+        int[][] matrix1 = new int[m][n];
+
+        System.out.println("Початкова матриця: ");
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                matrix1[i][j] = random.nextInt(10);
+
+                System.out.print(matrix1[i][j] + " ");
             }
+            System.out.println();
         }
 
-        System.out.println("Матриця M x N: " + array1);
+        int[][] transposedM = transposeM(matrix1);
 
+        System.out.println("Транспортована матриця: ");
 
+        for (int i = 0; i < transposedM.length; i++) {
+            for (int j = 0; j < transposedM[i].length; j++) {
+                System.out.print(transposedM[i][j] + " ");
+            }
+            System.out.println();
+        }
     }
 
+    public static int[][] transposeM(int[][] matrix1) {
+        int rows = matrix1.length;
+        int columns = matrix1[0].length;
+
+        int[][] transposedM = new int[columns][rows];
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < columns; j++) {
+                transposedM[j][i] = matrix1[i][j];
+            }
+        }
+        return transposedM;
+    }
 }
